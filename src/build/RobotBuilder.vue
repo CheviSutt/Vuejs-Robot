d10fc209ea69aa1f66e826dde55a4de8f7c0c7fa
chevisutton <cheviksutton@gmail.com>
2018-11-20 10:14:33 -0700

    module 6 befor this commit was actually saving data to api the this is >> return actions from promises

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -111,7 +111,8 @@ export default {
         + robot.torso.cost
         + robot.rightArm.cost
         + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }))
+        .then(() => this.$router.push('/cart')); // redirects to cart if fulfilled
       // avoiding pointers to the same object, good practice above | Object.assign
       this.addedToCart = true;
     },
