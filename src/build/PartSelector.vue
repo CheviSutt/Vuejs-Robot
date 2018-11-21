6f085a303ce87be04a4490c4bc377d2f27df4c90
chevisutton <cheviksutton@gmail.com>
2018-11-21 10:35:49 -0700

    Module 7 directive lifecycle hooks

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -11,7 +11,10 @@
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <!--<span v-pin:position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span>-->
-    <span v-pin="{ bottom: '10px', right: '5px' }" class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span
+      @click="pinPadding='30px'"
+      v-pin="{ bottom: pinPadding, right: pinPadding }" class="sale"
+      v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -44,7 +47,10 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return {
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+    };
   },
   computed: {
     selectedPart() {
--- src/shared/pin.directive.js
+++ src/shared/pin.directive.js
@@ -1,4 +1,4 @@
-// export default { // custom directive applied to PartSelector.vue, <span/sale
+// export default { // custom directive applied to PartSelector.vue, <span/sale // 1st example
 //   bind: (element, binding) => {
 //     // console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
 //     if (binding.arg !== 'position') return;
@@ -10,13 +10,25 @@
 //   },
 // };
 
-export default { // custom directive applied to PartSelector.vue, <span/sale
-  bind: (element, binding) => {
-    // console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
+// function applyStyle(element, binding) { // @nd example
+//   Object.keys(binding.value).forEach((position) => {
+//     element.style[position] = binding.value[position];
+//   });
+//   element.style.position = 'absolute';
+// }
+//
+// export default { // custom directive applied to PartSelector.vue, <span/sale
+//   bind: (element, binding) => {
+//     applyStyle(element, binding);
+//   },
+//   update: (element, binding) => {
+//     applyStyle(element, binding);
+//   },
+// };
 
-    Object.keys(binding.value).forEach((position) => {
-      element.style[position] = binding.value[position];
-    });
-    element.style.position = 'absolute';
-  },
-};
+export default function (element, binding) { // 3rd example
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = 'absolute';
+}
