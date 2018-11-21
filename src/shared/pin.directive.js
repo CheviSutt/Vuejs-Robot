6a8837f66cb65c896af4b275afc56b24aa1f26af
chevisutton <cheviksutton@gmail.com>
2018-11-21 09:50:37 -0700

    Module 7 passing data to  directives

--- src/shared/pin.directive.js
+++ src/shared/pin.directive.js
@@ -1,7 +1,22 @@
+// export default { // custom directive applied to PartSelector.vue, <span/sale
+//   bind: (element, binding) => {
+//     // console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
+//     if (binding.arg !== 'position') return;
+//
+//     Object.keys(binding.modifiers).forEach((key) => {
+//       element.style[key] = '5px';
+//     });
+//     element.style.position = 'absolute';
+//   },
+// };
+
 export default { // custom directive applied to PartSelector.vue, <span/sale
-  bind: (element) => {
+  bind: (element, binding) => {
+    // console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
+
+    Object.keys(binding.value).forEach((position) => {
+      element.style[position] = binding.value[position];
+    });
     element.style.position = 'absolute';
-    element.style.bottom = '5px';
-    element.style.right = '5px';
   },
 };
