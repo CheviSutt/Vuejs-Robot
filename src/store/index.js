0aece8ecbeb4e6867b9d408e17565ec1b0b7b5f1
chevisutton <cheviksutton@gmail.com>
2018-11-21 08:18:11 -0700

    module 6 Understanding Global and Namespaced State

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,16 @@ import usersModule from './modules/users';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  state: { // Root state, not in a namespaced module
+    foo: 'root-foo',
+  },
   modules: {
     robots: robotsModule,
     users: usersModule,
+  },
+  getters: {
+    // foo(state) {
+    //   return `root-getter/${state.foo}`; // value from the state property
+    // },
   },
 });
